e2a79f5375b766410b8f93694df5234ba01d8d2f
duyl97 <dl97.app@gmail.com>
2023-05-13 10:38:27 +0700

    init project

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0968d8c309285bfbc"
+  instance_type = "t2.micro"
+}
